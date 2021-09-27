8527cd767db2049e2bff2d1d9e3785d96231d193
sethclim <56656904+sethclim@users.noreply.github.com>
2021-09-27 15:54:45 -0400

    initialized project

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import Styles from "./styles.module.css";
+
+const Banner = () => {
+  return <div className={Styles.BannerBlue}>HIIII There</div>;
+};
+
+export default Banner;
