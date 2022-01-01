5b61259752a88cd12574cb3aaf5ac3336d6daab8
sethclim <56656904+sethclim@users.noreply.github.com>
2022-01-01 12:07:21 -0500

    Updated logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import Styles from "./styles.module.scss";
+import logo from "../../../public/assets/IMG/CSSLogo2.png"
 
 export default function Header() {
     return (
         <header className={Styles.header}>
             <div className={Styles.brandWrap}>
-                <img />
+                <img src={logo} />
                 <div className={Styles.brandName}>
                     <div className={Styles.mainName}>Climenhaga</div>
                     <p>Software Solutions</p>
