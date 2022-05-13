aedb6d65d5ef39282e50f13c0a4309a46a5fd0e4
sethclim <56656904+sethclim@users.noreply.github.com>
2022-05-12 22:05:46 -0400

    Updated Problem solve copy

--- src/components/Home/ProblemSolve/ProblemSolve.jsx
+++ src/components/Home/ProblemSolve/ProblemSolve.jsx
@@ -6,14 +6,14 @@ export default function ProblemSolve() {
         <div className={Styles.problemsolve}>
             <div className={Styles.card1}>
                 <div className={Styles.msgWrap}>
-                    <h3>Take your site to the next level</h3>
-                    <p>Breath life into your site</p>
+                    <h3>App Development</h3>
+                    <p>Test driven development process focused on creating reliable and maintainable software</p>
                 </div>
             </div>   
             <div className={Styles.card2}>
                 <div className={Styles.msgWrap}>
-                    <h3>Take your site to the next level</h3>
-                    <p>Breath life into your site</p>
+                    <h3>Design</h3>
+                    <p>User driven design for both intuitive operation and beautiful visuals</p>
                 </div>
             </div>   
             <div className={Styles.card3}>
